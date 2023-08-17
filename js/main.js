af875fc0f621a84aa5a30f3fe76300620fbfebff
nishantHolla <nishantholla19@gmail.com>
2023-08-17 22:05:25 +0530

    Fix formatting of negative one over results

--- js/main.js
+++ js/main.js
@@ -288,7 +288,7 @@ function makeOneOverButton(_DOM_ELEMENT, _text) {
 			equationTerm = `${evaluate(equation + equationTerm)}`
 
 		equation = ''
-		equationTerm = `${1/Number(equationTerm)}`
+		equationTerm = formatResult(`${1/Number(equationTerm)}`)
 		refreshCalculatorDisplay()
 	})
 }
